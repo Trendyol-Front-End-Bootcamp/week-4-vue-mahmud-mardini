34accd405fb232e06b4c97df262893b17fadd93f
mahmudmardini <M.Mardini152@gmail.com>
2021-07-22 19:21:12 +0300

    Setup a new VueJS app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app"
+       class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss">
+  @import "@/css/_style.scss";
+  @import "@/css/_main.scss";
+</style>
