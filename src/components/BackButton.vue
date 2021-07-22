7efc123888a6b207b4c40b58a155b70466efcf98
mahmudmardini <M.Mardini152@gmail.com>
2021-07-22 19:29:36 +0300

    add back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="back-btn">
+      <router-link to="/">
+        <img src="@/assets/back-button.png" alt="Starships">
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackButton'
+};
+</script>
+
+<style lang="scss">
+.back-btn {
+
+  img {
+    width: 64px;
+    height: 64px;
+  }
+}
+</style>
